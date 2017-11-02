d0da87b7ecea976c32d3804f5bda06a7b5a26df8
Tim Denison <tim.denison@gmail.com>
2017-11-01 18:25:24 -0700

    adds variable for local network exposure

--- src/utils/songUtils.js
+++ src/utils/songUtils.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 //import logo from '../../resources/man-singing.svg';
 import SetList from '../components/SetList';
 
+const karaokeApi = 'http://localhost:5000';//http://192.168.86.23:5000'
+
 export function renderSongList(sessionId, draggable) {
     return (    
         <div>
@@ -12,13 +14,13 @@ export function renderSongList(sessionId, draggable) {
 
   export async function getSession(){
 
-    return fetch('http://localhost:5000/api/Session')
+    return fetch(`${karaokeApi}/api/Session`)
     .then((response) => {return response.json()})
      .then((responseJson) => { return responseJson.sessionId});
 }
 
 export function authLogin(userName, password){
-  return fetch('http://localhost:5000/api/Authorization', {
+  return fetch(`${karaokeApi}/api/Authorization`, {
     method: 'POST',
     headers: {
       'Accept': 'application/json',
@@ -34,7 +36,7 @@ export function authLogin(userName, password){
 }
 
 export function createNewSessionIfNoneActive(){
-  return fetch('http://localhost:5000/api/Session', {
+  return fetch(`${karaokeApi}/api/Session`, {
     method: 'POST',
     headers:{
       'Accept': 'application/json',
@@ -46,14 +48,14 @@ export function createNewSessionIfNoneActive(){
 }
 
 export function getSessionSongs(sessionId){
-  return fetch('http://localhost:5000/api/Songs/'.concat(sessionId))
+  return fetch(`${karaokeApi}/api/Songs/`.concat(sessionId))
     .then((response) => response.json())
     .then((responseJson) =>  {return responseJson});
 }
 
 export function updateSongOrder(songs){
   var jsonBody = JSON.stringify(songs);
-  return fetch('http://localhost:5000/api/Setlist', {
+  return fetch(`${karaokeApi}/api/Setlist`, {
     method: 'POST',
     headers: {
       'Accept': 'application/json',
@@ -65,7 +67,7 @@ export function updateSongOrder(songs){
 
 export function addStageNameToSession(stageName, sessionId){
   console.log("adding stage name: ", stageName, sessionId);
-  return fetch('http://localhost:5000/api/StageNames', {
+  return fetch(`${karaokeApi}/api/StageNames`, {
     method: 'PUT',
     headers:{
       'Accept': 'application/json',
@@ -82,14 +84,14 @@ export function addStageNameToSession(stageName, sessionId){
 }
 
 export function getSessionStageNames(sessionId){
-  return fetch('http://localhost:5000/api/StageNames/'.concat(sessionId))
+  return fetch(`${karaokeApi}/api/StageNames/`.concat(sessionId))
     .then((response) => {return response.json()})
     .then((responseJson) =>  {return responseJson});
 }
 
 export function addSongToSession(song){
   //var jsonBody = JSON.stringify(song);
-  return fetch('http://localhost:5000/api/Songs', {
+  return fetch(`${karaokeApi}/api/Songs`, {
     method: 'POSt',
     headers:{
       'Accept': 'application/json',
